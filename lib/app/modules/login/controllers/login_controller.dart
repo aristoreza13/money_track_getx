9f4e57844156c0c07cbb5c2989a412b631b66ce8
aristoreza13 <rezaaristo04@gmail.com>
2024-01-09 15:09:03 +0700

    add login page

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,9 +1,33 @@
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:money_track_getx/app/routes/app_pages.dart';
+import 'package:money_track_getx/helpers/data_preferences.dart';
 
 class LoginController extends GetxController {
-  //TODO: Implement LoginController
+  final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
 
-  final count = 0.obs;
+  Future<void> handleSignInGoogle() async {
+    try {
+      GoogleSignInAccount? googleAccount = await _googleSignIn.signIn();
+      print(googleAccount!);
+      await DataPreferences.setEmail(googleAccount.email);
+      await DataPreferences.setDisplayName(googleAccount.displayName!);
+      await DataPreferences.setPhotoUrl(googleAccount.photoUrl!);
+      await DataPreferences.setLoggedIn();
+      Get.offAllNamed(Routes.NAVIGATOR);
+    } catch (error) {
+      print('Error signing in with Google: $error');
+      _googleSignIn.disconnect();
+    }
+  }
+
+  void loginUser(String email, password) async {
+    await DataPreferences.setEmail(email);
+    await DataPreferences.setPassword(password);
+    await DataPreferences.setLoggedIn();
+    Get.offAllNamed(Routes.NAVIGATOR);
+  }
+
   @override
   void onInit() {
     super.onInit();
@@ -18,6 +42,4 @@ class LoginController extends GetxController {
   void onClose() {
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:money_track_getx/app/routes/app_pages.dart';
 
 import '../../../../common/Colors.dart';
 import '../controllers/login_controller.dart';
@@ -11,6 +12,8 @@ class LoginView extends GetView<LoginController> {
   Widget build(BuildContext context) {
     TextEditingController emailC = TextEditingController();
     TextEditingController passwordC = TextEditingController();
+    final LoginController loginC = Get.find();
+
     return DecoratedBox(
       decoration: const BoxDecoration(
         gradient: LinearGradient(
@@ -102,7 +105,6 @@ class LoginView extends GetView<LoginController> {
               Container(
                 height: Get.height * 0.12,
                 width: Get.width,
-                // color: Colors.blueAccent,
                 margin: const EdgeInsets.symmetric(horizontal: 20),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -122,12 +124,12 @@ class LoginView extends GetView<LoginController> {
                         hintStyle: TextStyle(color: Colors.white),
                         border: OutlineInputBorder(
                           borderSide: BorderSide(
-                            color: Colors.white, // Warna border disini
+                            color: AppColors.whiteColor, // Warna border disini
                           ),
                         ),
                         focusedBorder: OutlineInputBorder(
                           borderSide: BorderSide(
-                            color: Colors.white, // Warna border disini
+                            color: AppColors.whiteColor, // Warna border disini
                           ),
                         ),
                         disabledBorder: OutlineInputBorder(
@@ -142,10 +144,13 @@ class LoginView extends GetView<LoginController> {
                         ),
                         enabledBorder: OutlineInputBorder(
                           borderSide: BorderSide(
-                            color: Colors.white, // Warna border disini
+                            color: AppColors.whiteColor, // Warna border disini
                           ),
                         ),
-                        suffixIcon: Icon(Icons.remove_red_eye_outlined),
+                        suffixIcon: Icon(
+                          Icons.remove_red_eye_outlined,
+                          color: AppColors.whiteColor,
+                        ),
                       ),
                       validator: (value) {
                         if (value!.isEmpty) {
@@ -163,7 +168,101 @@ class LoginView extends GetView<LoginController> {
                   ],
                 ),
               ),
-              Container(),
+              Container(
+                height: Get.height * 0.05,
+                width: Get.width,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                alignment: Alignment.centerLeft,
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Forgot Password?",
+                    style: TextStyle(color: AppColors.whiteColor),
+                  ),
+                ),
+              ),
+              SizedBox(height: Get.height * 0.1),
+              InkWell(
+                onTap: () async {
+                  // Aksi yang dijalankan saat tombol login ditekan
+                  // print('Tombol Login Ditekan');
+
+                  loginC.loginUser(emailC.text, passwordC.text);
+                },
+                child: Container(
+                  width: Get.width * 0.9,
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: AppColors.purpleMedium,
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: Get.height * 0.02),
+              Container(
+                height: 2,
+                width: Get.width,
+                color: AppColors.whiteColor,
+                margin: const EdgeInsets.symmetric(horizontal: 75),
+              ),
+              SizedBox(height: Get.height * 0.02),
+              Column(
+                children: [
+                  const Text("Atau login dengan", style: TextStyle(color: AppColors.whiteColor)),
+                  Container(
+                    margin: const EdgeInsets.only(top: 40, bottom: 20),
+                    padding: const EdgeInsets.only(left: 25, right: 25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            loginC.handleSignInGoogle();
+                          },
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Container(
+                              width: 40,
+                              height: 40,
+                              color: AppColors.whiteColor,
+                              child: Image.asset("assets/icons/google.png"),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Get.offAllNamed(Routes.HOME);
+                          },
+                          child: SizedBox(
+                            width: 40,
+                            height: 40,
+                            child: Image.asset("assets/icons/apple.png"),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Get.offAllNamed(Routes.HOME);
+                          },
+                          child: SizedBox(
+                            width: 40,
+                            height: 40,
+                            child: Image.asset("assets/icons/facebook.png"),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ],
           ),
         ),
