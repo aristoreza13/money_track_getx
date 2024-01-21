b1246571fa208eda1f486a745cd36f1cf07b1b08
aristoreza13 <rezaaristo04@gmail.com>
2024-01-21 19:15:49 +0700

    add new adapter

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:money_track_getx/app/data/models/user_data.dart';
 
 import 'app/data/models/transaction_item.dart';
 import 'app/routes/app_pages.dart';
@@ -12,6 +13,7 @@ void main() async {
   await DataPreferences.init();
   await Hive.initFlutter();
   Hive.registerAdapter(TransactionItemAdapter());
+  Hive.registerAdapter(UserDataAdapter());
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
